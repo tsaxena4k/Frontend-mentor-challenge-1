5ddbca856e3d53fa8f86764a3c065cd952e292b7
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-11-14 15:17:44 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <meta charset="UTF-8" />
+                <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
                 <title>Frontend Mentor | [Social media dashboard]</title>
