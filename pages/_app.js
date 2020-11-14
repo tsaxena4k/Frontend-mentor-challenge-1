b126e558386d7a10ea79b7d42828b57b55b642db
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-11-14 15:41:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <meta charset="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Frontend Mentor | [Social media dashboard]</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
