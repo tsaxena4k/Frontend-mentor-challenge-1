bfe6e13430d77ce5ba6da1ebf6cf1bcc1e321906
Tushar saxena <tsaxena3k@gmail.com>
2020-11-14 15:46:33 +0530

    Html Validation

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
             <h6 className="card-title"><i className="card-icons mr-2" style={{ color: social_icon_color }}>
               {social_icon === 'Facebook' ? <AiFillFacebook /> : (social_icon === 'Instagram' ? <AiOutlineInstagram /> : (social_icon === 'Twitter' ? <FaTwitter /> : <AiFillYoutube />))}
             </i>{social_handle}</h6>
-            <p className="card-text">{data}<h6>{data_text}</h6></p>
+            <div className="card-text"><p>{data}</p><h6>{data_text}</h6></div>
             <a href="#" className="card-link font-weight-bold" style={{ color: bottom_icon_color }}>{bottom_icon == 'Dropup' ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}{bottom_icon_data}</a>
           </div>
         </div>
@@ -48,46 +48,46 @@ export default function Home() {
   }
 
   return (
-      <main>
-        <div className="upper-container"></div>
-        <div className="lower-container">
-          <div className="container">
-            <div className="row header mb-5">
-              <div className="col-sm niche-border">
-                <h2>Social Media Dashboard</h2>
-                <h6>Total Followers:23,004</h6>
-              </div>
-              <div className="col-sm">
-                <label class="switch float-right">
-                  <input type="checkbox" onChange={() => document.body.classList.toggle('light-mode')} />
-                  <span class="slider round"></span>
-                </label>
-                <span className="mt-1 mr-3">Drak Mode</span>
-              </div>
+    <main>
+      <div className="upper-container"></div>
+      <div className="lower-container">
+        <div className="container">
+          <div className="row header mb-5">
+            <div className="col-sm niche-border">
+              <h2>Social Media Dashboard</h2>
+              <h6>Total Followers:23,004</h6>
             </div>
-            <div className="row mb-4">
-              {card_1_content.map(value => {
-                return card_style_1(value);
-              })}
+            <div className="col-sm">
+              <label class="switch float-right">
+                <input type="checkbox" onChange={() => document.body.classList.toggle('light-mode')} />
+                <span class="slider round"></span>
+              </label>
+              <span className="mt-1 mr-3">Drak Mode</span>
             </div>
-            <div className="row mb-2">
-              <div className="col-12 mb-3">
-                <h3>Overview-Today</h3>
-              </div>
-              {card_2_content.map(value => {
-                return card_style_2(value);
-              })}
+          </div>
+          <div className="row mb-4">
+            {card_1_content.map(value => {
+              return card_style_1(value);
+            })}
+          </div>
+          <div className="row mb-2">
+            <div className="col-12 mb-3">
+              <h3>Overview-Today</h3>
             </div>
-            <div className="row">
-              <div className="col-sm text-center">
-                <div className="attribution">
-                  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            {card_2_content.map(value => {
+              return card_style_2(value);
+            })}
+          </div>
+          <div className="row">
+            <div className="col-sm text-center">
+              <div className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
                   Coded by <a href="https://github.com/tsaxena4k" target="_blank">Tushar Saxena</a>.
                 </div>
-              </div>
             </div>
           </div>
         </div>
-      </main>
+      </div>
+    </main>
   )
 }
